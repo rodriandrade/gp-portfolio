3bc5327dff53a4d3bb268785b8eeff1b2810ca37
rodriandrade <rodriandrade1498@gmail.com>
2021-02-01 01:51:28 -0300

    Menu

--- containers/Home/index.js
+++ containers/Home/index.js
@@ -86,7 +86,7 @@ const Home = () =>{
     return(
         <>
             <Inner>
-                
+                <NavMenu /> 
                 <Hero /> 
                 <AboutCard />  
                 <Services /> 
