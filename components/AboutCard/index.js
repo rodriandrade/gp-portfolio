4ba2be312a5631cceb420cb95f264cfa5c56d527
rodriandrade <rodriandrade1498@gmail.com>
2021-02-01 01:29:18 -0300

    Phone Message Added

--- components/AboutCard/index.js
+++ components/AboutCard/index.js
@@ -62,7 +62,9 @@ const AboutCard = () =>{
                                 <a href="mailto:gabrielpiantanida16@gmail.com">
                                     <JobCard icon="/email.svg" job="Email" company="gabrielpiantanida16@gmail.com"/>
                                 </a>
+                                <a href="https://wa.me/5491141896430">
                                 <JobCard icon="/phone.svg" job="Teléfono" company="1141896430"/>
+                                </a>
                             </ContainerJobs>
                             <a href="/cv-piantanida-1.pdf" target="blank"><Button>Ver CV</Button></a>
                         </ContainerText>
