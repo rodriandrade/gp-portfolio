97b4aca35c2420a158783dd71905f0d9f3737cc8
rodriandrade <rodriandrade1498@gmail.com>
2021-03-03 00:46:34 -0300

    video update

--- components/Video/index.js
+++ components/Video/index.js
@@ -5,16 +5,13 @@ const Video = props => {
 
     const {video} = props.data
     //<p>Transmisiones por "Frecuencia Fútbol" Am Con Vos 1420 x "Secta Deportiva" canal de Youtube</p>
-    console.log(video);
     return(
         <Col desktop={4} tablet={6} mobile={12}>
             <div>
                 {video.includes("https") ? 
                     <VideoYouTube src={video} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></VideoYouTube>
                     : 
-                    
-                        <VideoLocal controls><source src="/gol_de_jorginho.mp4" type="video/mp4" /></VideoLocal>
-                    
+                    <VideoLocal controls><source src={video} type="video/mp4" /></VideoLocal>
                 }
             </div>
         </Col>
